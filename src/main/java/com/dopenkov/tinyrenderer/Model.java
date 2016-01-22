0a6901da0b00dd6372226a51e299d69c6ea505e3
Dmitry Openkov <dimaopen@gmail.com>
2016-01-22 09:27:22 +0300

    replaced parsing with Scanner

--- src/main/java/com/dopenkov/tinyrenderer/Model.java
+++ src/main/java/com/dopenkov/tinyrenderer/Model.java
@@ -2,12 +2,14 @@ package com.dopenkov.tinyrenderer;
 
 import com.dopenkov.tinyrenderer.vectormath.Vector3;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
+import java.util.Locale;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
@@ -29,66 +31,62 @@ public class Model {
 
     public Model(InputStream objStream) {
         faces = new ArrayList<>(1024);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(objStream))) {
+        try (Scanner scanner = new Scanner(objStream)) {
+            scanner.useDelimiter("\\s+|/");
+            scanner.useLocale(Locale.US);
             List<Vector3> vertexes = new ArrayList<>(1024);
             List<Vector3> normals = new ArrayList<>(1024);
             List<Vector3> uvs = new ArrayList<>(1024);
-            Pattern vectorPattern = Pattern.compile("^(v|(vn)|(vt))\\s+(.+)");
-            Pattern facePattern = Pattern.compile("^f\\s+(.+)");
-            for (String line; (line = br.readLine()) != null; ) {
-                Matcher m = vectorPattern.matcher(line);
-                if (m.find()) {
-                    //do vector parsing
-                    Vector3 vec = new Vector3(m.group(4));
-
-                    final String letter = m.group(1);
-                    switch (letter) {
-                        case "v":
-                            vertexes.add(vec);
-                            break;
-                        case "vn":
-                            normals.add(vec);
-                            break;
-                        case "vt":
-                            uvs.add(vec);
-                            break;
-                    }
-                    continue;
-                }
-                Matcher fm = facePattern.matcher(line);
-                if (fm.find()) {
-                    final String faceData = fm.group(1);
-                    int i = 0;
-                    final Vertex[] face = new Vertex[3];
-                    for (StringTokenizer tokenizer = new StringTokenizer(faceData); tokenizer.hasMoreTokens(); i++) {
-                        String s = tokenizer.nextToken();
-                        String[] numbers = s.split("/");
-                        Vertex v = new Vertex();
-                        try {
-                            v.location = vertexes.get(Integer.parseInt(numbers[0]) - 1);
-                            v.uv = uvs.get(Integer.parseInt(numbers[1]) - 1);
-                            v.normal = normals.get(Integer.parseInt(numbers[2]) - 1);
-                            face[i] = v;
-                        } catch (Exception e) {
-                            throw new RuntimeException("Cannot parse face from string " + faceData, e);
+            Pattern v = Pattern.compile("v");
+            Pattern vn = Pattern.compile("vn");
+            Pattern vt = Pattern.compile("vt");
+            Pattern f = Pattern.compile("f");
+            int lineNum = 1;
+            while (scanner.hasNextLine()) {
+                try {
+                    if (scanner.hasNext(v)) {
+                        vertexes.add(readVector(scanner));
+                    } else if (scanner.hasNext(vn)) {
+                        normals.add(readVector(scanner));
+                    } else if (scanner.hasNext(vt)) {
+                        uvs.add(readVector(scanner));
+                    } else if (scanner.hasNext(f)) {
+                        scanner.next();
+                        final Vertex[] face = new Vertex[3];
+                        for (int i = 0; i < 3; i++) {
+                            Vertex vertex = new Vertex();
+                            vertex.location = vertexes.get(scanner.nextInt() - 1);
+                            vertex.uv = uvs.get(scanner.nextInt() - 1);
+                            vertex.normal = normals.get(scanner.nextInt() - 1);
+                            face[i] = vertex;
                         }
+                        faces.add(face);
                     }
-                    faces.add(face);
+                } catch (Exception e) {
+                    throw new RuntimeException("Cannot parse line " + lineNum);
                 }
+                scanner.nextLine();
+                lineNum++;
             }
             System.out.println("vertexes = " + vertexes.size());
             System.out.println("normals = " + normals.size());
             System.out.println("uvs = " + uvs.size());
             System.out.println("faces = " + faces.size());
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot read from stream", e);
         }
         /*BufferedImage img = null;
         try {
-            img = ImageIO.read(new File("/Users/Dmitry/Projects/samples/tinyrenderer-tutorial/obj/african_head_diffuse.tga"));
+            img = ImageIO.read(new File("test01.tga"));
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+    }
+
+    private Vector3 readVector(Scanner scanner) {
+        scanner.next();
+        float x = scanner.hasNextFloat() ? scanner.nextFloat() : 0;
+        float y = scanner.hasNextFloat() ? scanner.nextFloat() : 0;
+        float z = scanner.hasNextFloat() ? scanner.nextFloat() : 0;
+        return new Vector3(x, y, z);
     }
 
 
--- src/test/java/com/dopenkov/tinyrenderer/ModelTest.java
+++ src/test/java/com/dopenkov/tinyrenderer/ModelTest.java
@@ -3,6 +3,8 @@ package com.dopenkov.tinyrenderer;
 import com.dopenkov.tinyrenderer.vectormath.Vector3;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -14,13 +16,18 @@ public class ModelTest {
     @Test
     public void testReadingObj() {
         Model modelT = new Model(this.getClass().getResourceAsStream("/model/test_model.obj"));
-        assertEquals(50054, modelT.getFaces().size());
-        Model.Vertex[] face0 = modelT.getFaces().get(0);
+        List<Model.Vertex[]> faces = modelT.getFaces();
+        assertEquals(50054, faces.size());
+        Model.Vertex[] face0 = faces.get(0);
         assertEquals(3, face0.length);
         assertEquals(new Vector3(0.028911f,  0.137885f, 0.150147f), face0[0].location);
         assertEquals(new Vector3(0.122546f, 0.262678f), face0[0].uv);
         assertEquals(new Vector3(0.124554f, 0.265355f), face0[1].uv);
         assertEquals(new Vector3(-0.589900f, 0.453900f, 0.667800f), face0[2].normal);
+
+        Model.Vertex[] faceLast = faces.get(faces.size() - 1);
+        assertEquals(3, faceLast.length);
+        assertEquals(new Vector3(0.205570f, -0.159949f, 0.068638f), faceLast[2].location);
     }
 
 }
